9a3a1f7a9afe2f615b4a2f9af735f3989d1fc3c7
Arthur Mckellar <micoolplays@gmail.com>
2024-10-08 00:40:53 +0000

    Oct 8, 2024, 1:40 PM

--- Lab8/quicksort_trials.py
+++ Lab8/quicksort_trials.py
@@ -53,8 +53,8 @@ def times_vs_size(list_of_ns, n_trials=50, pivot_type=LEFT_PIVOT):
 # initially [40, 80, 120 ... 760]
 # list_of_ns will be used as the x-axis on your graphs
 # and average time will be plotted on the y-axis
-list_of_ns = list(range(40, 800, 20))
-
+# list_of_ns = list(range(40, 800, 20))
+list_of_ns = list(random.sample(range(40, 800), 100))
 # some quick and dirty plotting
 n_trials = 100
 avg_times_LEFT = times_vs_size(list_of_ns, n_trials, pivot_type=LEFT_PIVOT)
