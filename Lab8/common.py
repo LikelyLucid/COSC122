2dc4aa4e521276670a4ccb2ad5f49ce0105cfb05
Arthur Mckellar <micoolplays@gmail.com>
2024-10-08 02:12:12 +0000

    Oct 8, 2024, 3:12 PM

--- Lab8/common.py
+++ Lab8/common.py
@@ -2,14 +2,14 @@ from quicksort import *
 
 
 def read_data(filename):
-    """ Returns a list of integers read from the file """
+    """Returns a list of integers read from the file"""
     with open(filename) as infile:
         values = [int(line.strip()) for line in infile]
     return values
 
 
 def common_items(list_x, list_y):
-    """ Takes two sorted lists as input (ie, both lists are in ascending order).
+    """Takes two sorted lists as input (ie, both lists are in ascending order).
     Returns a list containing all the items in list_x that are also in list_y.
     Returns an empty list if there are none.
 
@@ -54,16 +54,27 @@ def common_items(list_x, list_y):
     []
     >>> common_items([],[])
     []
+    >>> common_items([0,1,2,3],[0,0,2,4])
+    [0, 2]
+    >>> common_items([0,1,2,2,5,5,6,6,7],[0,0,2,4,5,5,5,7])
+    [0, 2, 5, 7]
     """
-    # add the following doctests (and some of your own)
-    # when ready for lists of non-unique items
-    # >>> common_items([0,1,2,3],[0,0,2,4])
-    # [0, 2]
-    # >>> common_items([0,1,2,2,5,5,6,6,7],[0,0,2,4,5,5,5,7])
-    # [0, 2, 5, 7]
-
     # ---start student section---
-    pass
+    index_x, index_y = 0, 0
+    result = []
+
+    while index_x < len(list_x) and index_y < len(list_y):
+        if list_x[index_x] < list_y[index_y]:
+            index_x += 1
+        elif list_x[index_x] > list_y[index_y]:
+            index_y += 1
+        else:
+            if len(result) == 0 or result[-1] != list_x[index_x]:
+                result.append(list_x[index_x])
+            index_x += 1
+            index_y += 1
+
+    return result
     # ===end student section===
 
 
