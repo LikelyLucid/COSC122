7cd4578fad5aea606e2f1d8487a2592b54cd3dcd
Arthur Mckellar <micoolplays@gmail.com>
2024-10-12 03:39:30 +0000

    Oct 12, 2024, 4:39 PM

--- Lab9/heaps.py
+++ Lab9/heaps.py
@@ -162,8 +162,8 @@ class MinHeap(Heap):
             return self._items.pop()
         min_item = self._items[1]
         self._items[1] = self._items.pop()
+
         self._sift_down(1)
-        return min_item
         # ===end student section===
 
     # -------------------------------------------------
